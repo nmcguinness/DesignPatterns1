0138abca17f2dc3d3782dd9cd962cc111bba8fa2
Niall McGuinness <niall.mcguinness@dkit.ie>
2026-02-13 12:04:43 +0000

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,6 @@ public class Main {
         * 2. PECS
         * 3. DP1 - Strategy/Command
         * */
-
         //create instance to call instance methods and access instance variables
         Main theApp = new Main();
         theApp.run();
@@ -18,7 +17,22 @@ public class Main {
 
     public void run(){
          List<String> dataList = new ArrayList<>(List.of("Ana","Ben","Ciara","Dave","Erica"));
-
          //write the code to output the average string length
+    }
+
+//    public double getAverage(List<String> data)  {
+//        return -1;
+//    }
+
+    public double getAverage(List<String> data)  {
+
+        if(data == null || data.size() == 0)
+            throw new IllegalArgumentException("data is null or zero length!");
+
+        double lengthSum = 0;
+        for(String s : data){
+            lengthSum += s.length();
+        }
+        return lengthSum/data.size();
     }
 }
