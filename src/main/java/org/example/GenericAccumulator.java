0666b05cfc33758df8a5faa3530cd3e27958fb04
Niall McGuinness <niall.mcguinness@dkit.ie>
2026-02-18 12:14:15 +0000

    Added GenericAccumulator

--- src/main/java/org/example/GenericAccumulator.java
+++ src/main/java/org/example/GenericAccumulator.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.util.function.Function;
+
 public class GenericAccumulator {
-    private double min;
-    private double max;
+    private double min = Double.POSITIVE_INFINITY;
+    private double max = Double.NEGATIVE_INFINITY;
     private double mean, sum;
     private int count;
 
@@ -11,8 +13,10 @@ public class GenericAccumulator {
     public double getMax() {return max;}
     public double getMean() {return mean;}
 
-    public void update(String value){
-        double measure = value.length();
+    public void reset() {min = max = mean = sum = 0; count = 0;}
+
+    public <T> void update(T value, Function<T, Double> measurer){
+        double measure = measurer.apply(value).doubleValue();
         //min
         if(measure < min) min = measure;
         //max
